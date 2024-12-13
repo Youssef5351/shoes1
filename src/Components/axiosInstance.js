d735fafa997f383bc102f15b3cb7dd34a2059718
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-13 23:48:42 +0200

    Update axiosInstance.js

--- src/Components/axiosInstance.js
+++ src/Components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://shoes-inky-seven.vercel.app/api', // Use the base URL for your API
+  baseURL: 'https://shoes-inky-seven.vercel.app', // Use the base URL for your API
   headers: {
     'Content-Type': 'application/json',
   },
